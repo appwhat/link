492fe5f1c362433ef522eb58036118fa406efa49
Leandro Cullen <73318882+appwhat@users.noreply.github.com>
2020-12-02 22:39:42 -0300

    Update isGd.js

--- javascript/isGd.js
+++ javascript/isGd.js
@@ -1,26 +1,29 @@
 $(document).ready(function () {
-   
-    $("#buttonEncurtarISGD").click(function () {
-         if(linkCompleto2.value == ""){
-        console.log("Campo Invalido")
-    }else{
-        var longoURL = linkCompleto2.value
 
-        $.getJSON("https://is.gd/create.php?callback=?", {
-            url: longoURL,
-            format: "json"
-        }).done(function (data) {
-            linkCurtoISGD.value = data.shorturl
-            $("#divLinkCopiarIsGd").slideDown("slow")
-            AcessarLinkEncurtadoISGD.href = data.shorturl
-        })
-    }
+    $("#buttonEncurtarISGD").click(function () {
+        if (linkCompleto2.value == "" || linkCompleto2.value.length < 6) {
+            console.log("URL Invalida")
+        } else {
+            var longoURL = linkCompleto2.value
+
+            $.getJSON("https://is.gd/create.php?callback=?", {
+                url: longoURL,
+                format: "json"
+            }).done(function (data) {
+                if(data.shorturl == undefined){
+                    console.log("URL Invalida")
+                }else{
+                linkCurtoISGD.value = data.shorturl
+                $("#divLinkCopiarIsGd").slideDown("slow")
+                AcessarLinkEncurtadoISGD.href = data.shorturl}
+            })
+        }
     })
 
-    $("#copiarTimiLinkISGD").click(function(){
+    $("#copiarTimiLinkISGD").click(function () {
         $("#linkCurtoISGD").select()
         document.execCommand('copy')
     })
-    
+
 
 })
