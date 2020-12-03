d3181fe1836c068b2a42d14ea05e3bbed4fe24b4
Leandro Cullen <73318882+appwhat@users.noreply.github.com>
2020-12-02 22:40:32 -0300

    Update tinyApi.js

--- javascript/tinyApi.js
+++ javascript/tinyApi.js
@@ -1,13 +1,17 @@
 $(document).ready(function () {
     $("#buttonEncurtarTINYLINK").click(function () {
         /* click on Button */
+
+        if(linkCompletoTINYLINK.value == "" || linkCompletoTINYLINK.value.length < 6){
+            console.log("URL Invalida")
+        }else{
         var fullLink = linkCompletoTINYLINK.value
         $.get(`https://tinyurl.com/api-create.php?url=${fullLink}`, function (shorturl) {
             var linkShortTINYLINK = shorturl
             linkCurtoTiny.value = linkShortTINYLINK
             AcessarLinkEncurtado.href = linkShortTINYLINK   
         $("#divLinkCopiarTINYLINK").slideDown("slow")
-        })
+        })}
     })
     /* Copy Link Final */
     $("#copiarTINYLINK").click(function(){
