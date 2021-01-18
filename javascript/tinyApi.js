43fa0336fd83342705e1e19b7e0db5f1a9ea2536
Leandro Cullen <73318882+appwhat@users.noreply.github.com>
2021-01-18 18:45:35 -0300

    Update tinyApi.js

--- javascript/tinyApi.js
+++ javascript/tinyApi.js
@@ -1,1 +1,21 @@
-$(document).ready(function(){$("#buttonEncurtarTINYLINK").click(function(){if(""==linkCompletoTINYLINK.value||linkCompletoTINYLINK.value.length<6)console.log("URL Invalida");else{var n=linkCompletoTINYLINK.value;$.get(`https://tinyurl.com/api-create.php?url=${n}`,function(n){var o=n;linkCurtoTiny.value=o,AcessarLinkEncurtado.href=o,$("#divLinkCopiarTINYLINK").slideDown("slow")})}}),$("#copiarTINYLINK").click(function(){$("#linkCurto").select(),document.execCommand("copy")})});
+$(document).ready(function() {
+    $("#buttonEncurtarTINYLINK").click(function() {
+        if (linkCompletoTINYLINK.value == "" || linkCompletoTINYLINK.value.length < 6) {
+            /* Tratamento de Erro */
+            $("#divErro").text(`Por favor, forneça um url válido.`).removeClass("d-none").slideDown("3000").delay("3000").slideUp("3000");
+            console.log("Por favor, forneça um url válido.");
+        } else {
+            var fullLink = linkCompletoTINYLINK.value;
+            $.get(`https://tinyurl.com/api-create.php?url=${fullLink }`, function(fullLink) {
+                var linkShortTINYLINK = fullLink;
+                linkCurtoTiny.value = linkShortTINYLINK;
+                AcessarLinkEncurtado.href = linkShortTINYLINK;
+                $("#divLinkCopiarTINYLINK").slideDown("slow")
+            })
+        }
+    })
+    $("#copiarTINYLINK").click(function() {
+        $("#linkCurtoTiny").select();
+        document.execCommand("copy")
+    })
+});
