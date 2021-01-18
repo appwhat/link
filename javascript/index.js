d8bccddd595fbb883b19fc7cf1fd014f9f7e7345
Leandro Cullen <73318882+appwhat@users.noreply.github.com>
2021-01-18 18:44:12 -0300

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,31 @@
-function getLinkWA(){var e=document.querySelector("#number").value;if(e.length<9)numberErro.classList.add("text-danger");else{numberErro.classList.remove("text-danger"),resultadolink.classList.add("d-block"),copiar.classList.add("d-block"),copyIdShort.classList.add("d-block"),(e=e.replace(/\D/g,"")).length<13&&(e="55"+e);var n=document.querySelector("#menssagem").value;let t=`https://api.whatsapp.com/send?phone=${e}&text=${n=window.encodeURIComponent(n)}`;linkfinal.innerHTML=t,iniciarconversa.href=t}}
+function getLinkWA() {
+    /* Campo Numero */
+    var numero = document.querySelector("#number").value;
+    /* Verificando se número e menor que 9 */
+    if (numero.length < 9)
+    /* Se for menor que 9 faça isso */
+        numberErro.classList.add("text-danger");
+    else {
+        /* Se for maior que 9 faça isso */
+        numberErro.classList.remove("text-danger"),
+            resultadolink.classList.add("d-block"),
+            copiar.classList.add("d-block"),
+            copyIdShort.classList.add("d-block"),
+            numero = numero.replace(/\D/g, "");
+        /* Verificando se numero e menor que 13*/
+        if (numero.length < 13) {
+            /* Caso não! Adicione 55 código do Pais  */
+            numero = "55" + numero;
+        }
+        /* Campo de mensagem */
+        var texto = document.querySelector("#menssagem").value;
+        /*  codifica uma URL */
+        texto - window.encodeURIComponent(texto);
+        /* API do whatsapp */
+        let resultado = `https://api.whatsapp.com/send?phone=${numero}&text=${texto}`;
+        /* Link final codificado e pronto para usso */
+        linkfinal.innerHTML = resultado;
+        /* Botão iniciar conversa com o link acima */
+        iniciarconversa.href = resultado;
+    }
+}
