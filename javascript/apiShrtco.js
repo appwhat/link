6694c001e20e3346f8cbc39ef34d03677b2d94b5
Leandro Cullen <73318882+appwhat@users.noreply.github.com>
2021-01-18 18:43:50 -0300

    Update apiShrtco.js

--- javascript/apiShrtco.js
+++ javascript/apiShrtco.js
@@ -1,55 +1,53 @@
-$(function () {
-    $("#buttonEncurtarShrtCode").click(function () {
+$(function() {
+    $("#buttonEncurtarShrtCode").click(function() {
         var fullLink = linkCompleto3.value
         if (fullLink === "" || linkCompleto3.value.length < 8) {
-            console.log("URL invalidade.")
+            /* Tratamento de Erro */
+            $("#divErro").text(`Por favor, forneça um url válido.`).removeClass("d-none").slideDown("slow").delay("3000").slideUp("3000")
+            console.log("URL Inválida")
         } else {
             if (opcao1.checked) {
-                $.getJSON(`https://api.shrtco.de/v2/shorten?url=${fullLink}`, function (data) {
+                $.getJSON(`https://api.shrtco.de/v2/shorten?url=${fullLink}`, function(data) {
                     var linkShortShrtCode = data.result.short_link
 
                     linkCurtoShrtCode.value = linkShortShrtCode
                     AcessarLinkEncurtadoShrtCode.href = "https://" + linkShortShrtCode
 
                     $("#divLinkCopiarShrtCode").slideDown("slow")
-                    /* 
-                     console.log(data.result.short_link2)
-                     console.log(data.result.short_link3) */
+                        /* console.log(data.result.short_link2)
+                        console.log(data.result.short_link3) */
 
-
-                    /* Copiar Link Encurtado */
-                   
                 })
             } else if (opcao2.checked) {
-                $.getJSON(`https://api.shrtco.de/v2/shorten?url=${fullLink}`, function (data) {
+                $.getJSON(`https://api.shrtco.de/v2/shorten?url=${fullLink}`, function(data) {
                     var linkShortShrtCode = data.result.short_link2
 
                     linkCurtoShrtCode.value = linkShortShrtCode
                     AcessarLinkEncurtadoShrtCode.href = "https://" + linkShortShrtCode
 
                     $("#divLinkCopiarShrtCode").slideDown("slow")
-                    /* 
-                     console.log(data.result.short_link2)
-                     console.log(data.result.short_link3) */
+                        /* 
+                         console.log(data.result.short_link2)
+                         console.log(data.result.short_link3) */
 
 
                 })
             } else if (opcao3.checked) {
-                $.getJSON(`https://api.shrtco.de/v2/shorten?url=${fullLink}`, function (data) {
+                $.getJSON(`https://api.shrtco.de/v2/shorten?url=${fullLink}`, function(data) {
                     var linkShortShrtCode = data.result.short_link3
 
                     linkCurtoShrtCode.value = linkShortShrtCode
                     AcessarLinkEncurtadoShrtCode.href = "https://" + linkShortShrtCode
 
                     $("#divLinkCopiarShrtCode").slideDown("slow")
-                    /* 
-                     console.log(data.result.short_link2)
-                     console.log(data.result.short_link3) */
+                        /* 
+                         console.log(data.result.short_link2)
+                         console.log(data.result.short_link3) */
 
 
                 })
             }
-            $("#copiarTimiLinkShrtCode").click(function () {
+            $("#copiarTimiLinkShrtCode").click(function() {
                 $("#linkCurtoShrtCode").select();
                 document.execCommand('copy')
             })
@@ -58,10 +56,3 @@ $(function () {
     })
 
 })
-
-/* .done(function () {
-           console.log("Encurtadas")
-        }).fail(function (error_code, error, readyState) {
-           console.log("URL inválido enviado")
-           console.log(error_code.responseJSON.error)
-        }) */
